0a8d8bc69c65d742798e93cc09aa0f1625fa4046
BZLZHH <admin@bzlzhh.top>
2026-04-11 11:38:27 +0800

    [Fix] (DisplayList): Fix vertex pointer functions and glDrawArrays execution in display lists.
    - Add LIST_RECORD support to glVertexPointer, glNormalPointer, glColorPointer, glTexCoordPointer, glIndexPointer
    - Remove early return in glDrawArrays when display list is calling
    - Ensure vertex data is properly recorded and replayed in display lists

--- SimpleFPEWrapper/fpe/vertexpointer.cpp
+++ SimpleFPEWrapper/fpe/vertexpointer.cpp
@@ -13,6 +13,8 @@
 #define DEBUG 0
 
 void glVertexPointer(GLint size, GLenum type, GLsizei stride, const void* pointer) {
+    LIST_RECORD(glVertexPointer, {}, size, type, stride, pointer)
+
     // LOG_D("glVertexPointer, size = %d, type = %s, stride = %d, pointer = 0x%x", size, glEnumToString(type), stride,
     // pointer)
     auto& attr = g_glstate.fpe_state.vertexpointer_array.attributes[vp2idx(GL_VERTEX_ARRAY)];
@@ -28,6 +30,8 @@ void glVertexPointer(GLint size, GLenum type, GLsizei stride, const void* pointe
 }
 
 void glNormalPointer(GLenum type, GLsizei stride, const GLvoid* pointer) {
+    LIST_RECORD(glNormalPointer, {}, type, stride, pointer)
+
     // LOG_D("glNormalPointer, type = %s, stride = %d, pointer = 0x%x", glEnumToString(type), stride, pointer)
     g_glstate.fpe_state.vertexpointer_array.attributes[vp2idx(GL_NORMAL_ARRAY)] = {
         .size = 3,
@@ -42,6 +46,8 @@ void glNormalPointer(GLenum type, GLsizei stride, const GLvoid* pointer) {
 }
 
 void glColorPointer(GLint size, GLenum type, GLsizei stride, const GLvoid* pointer) {
+    LIST_RECORD(glColorPointer, {}, size, type, stride, pointer)
+
     // LOG_D("glColorPointer, size = %d, type = %s, stride = %d, pointer = 0x%x", size, glEnumToString(type), stride,
     // pointer)
     g_glstate.fpe_state.vertexpointer_array.attributes[vp2idx(GL_COLOR_ARRAY)] = {
@@ -57,6 +63,8 @@ void glColorPointer(GLint size, GLenum type, GLsizei stride, const GLvoid* point
 }
 
 void glTexCoordPointer(GLint size, GLenum type, GLsizei stride, const GLvoid* pointer) {
+    LIST_RECORD(glTexCoordPointer, {}, size, type, stride, pointer)
+
     // LOG_D("glTexCoordPointer, size = %d, type = %s, stride = %d, pointer = 0x%x", size, glEnumToString(type), stride,
     // pointer) LOG_D("Active texture: %s", glEnumToString(g_glstate.fpe_state.client_active_texture))
     g_glstate.fpe_state.vertexpointer_array.attributes[vp2idx(GL_TEXTURE_COORD_ARRAY)] = {
@@ -72,6 +80,8 @@ void glTexCoordPointer(GLint size, GLenum type, GLsizei stride, const GLvoid* po
 }
 
 void glIndexPointer(GLenum type, GLsizei stride, const GLvoid* pointer) {
+    LIST_RECORD(glIndexPointer, {}, type, stride, pointer)
+
     // LOG_D("glIndexPointer, size = %d, type = %s, stride = %d, pointer = 0x%x", glEnumToString(type), stride, pointer)
     g_glstate.fpe_state.vertexpointer_array.attributes[vp2idx(GL_INDEX_ARRAY)] = {
         .size = 1,
